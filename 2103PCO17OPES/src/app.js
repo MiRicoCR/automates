c00e4618d3af52c143f5857a352e023b6637331c
MiRicoCR <misael.rico@cassavaroots.com>
2021-03-22 18:44:45 -0600

    algo

--- 2103PCO17OPES/src/app.js
+++ 2103PCO17OPES/src/app.js
@@ -5,14 +5,13 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')))
-app.set('views', path.join(__dirname, 'public/views'))
-app.set('view engine', 'pug')
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'public/Views'));
+app.set('view engine', 'pug');
 
 app.use(morgan('dev'));
 app.use(helmet());
 
-console.log(__dirname)
 
 app.get('/', (req, res) => {
     res.render('index');
