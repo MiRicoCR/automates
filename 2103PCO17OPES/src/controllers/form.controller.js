d115b1f032e56d7d7ec2a616483141f04c708ef5
MiRicoCR <misael.rico@cassavaroots.com>
2021-03-30 18:46:00 -0600

    avance

--- 2103PCO17OPES/src/controllers/form.controller.js
+++ 2103PCO17OPES/src/controllers/form.controller.js
@@ -1,3 +1,5 @@
+const randomizer = require("random-seed-weighted-chooser").default;
+
 exports.instanceQuizz = (req, res) => {
     console.log(req.params.store);
     res.render('quizz.html');
@@ -5,6 +7,28 @@ exports.instanceQuizz = (req, res) => {
 
 exports.insertData = (req, res) => {
     console.log(req.body);
-    res.render('reward.html');
-    res.end();
+    res.json({data: 'reward'});
+};
+
+function generateID(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+ }
+
+exports.showReward = (req, res) => {
+    let iceCreamFlavors = [
+        { flavor: "Bebida pupi gratis", weight: 0.15 },
+        { flavor: "15% de descuento", weight: 0.725 },
+        { flavor: "Topping gratis", weight:  9.125}
+      ];
+
+    let premio = randomizer.chooseWeightedObject(iceCreamFlavors);
+    console.log(premio);
+
+    res.render('reward.html', {reward: premio.flavor,code: generateID(6)});
 };
